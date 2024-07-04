3aae27a389cc677bede270723fec5d3b9c50fa68
Rustem <mrustem501@gmail.com>
2024-07-04 04:17:13 -0700

    Character Basic Skeleton

---
+++ prototype 1/Assets/Rustem/Character.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    //private CharacterMovement _movement; 
+    //private CharachetDebug _debug;
+    //private CharacterInteract _interact;
+    //private CharacterShoot _shoot;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // _interact.Interact();
+    }
+
+    void Start()
+    {
+        // TODO Initialization
+        // = new 
+    }
+
+    
+    void Update()
+    {
+        //_movement.Move();
+        //_debug.PrintMessage();
+        //_shoot.Shoot();
+    }
+}
